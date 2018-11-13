03e16f4a014ca55124c362bd3267857328803977
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2018-11-13 16:21:43 -0500

    Add route for item POST.

--- routes/api/items.js
+++ routes/api/items.js
@@ -4,9 +4,9 @@ const router = express.Router();
 // Item Model
 const Item = require('../../models/item');
 
-// @route GET api/items
-// @desc Get All Items
-// @access Public
+// @route   GET api/items
+// @desc    Get All Items
+// @access  Public
 router.get('/', (request, response) => {
     Item.find()
         .sort({
@@ -15,5 +15,17 @@ router.get('/', (request, response) => {
         .then(items => response.json(items));
 });
 
+// @route   POST api/items
+// @desc    Create an Item
+// @access  Public
+router.post('/', (request, response) => {
+    const newItem = new Item({
+        name: request.body.name,
+    });
+
+    newItem.save()
+        .then(item => response.json(item))
+        .catch(error => console.log(error));
+});
 
 module.exports = router;
